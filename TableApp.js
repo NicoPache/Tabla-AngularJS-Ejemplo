8d21adbe528077f832e214d55e84311095b849bc
NicoPache <nepacheco94@gmail.com>
2022-06-21 20:06:40 -0300

    ejemplo tabla

---
+++ TableApp.js
@@ -0,0 +1,26 @@
+var app = angular.module('tablaModule',['ngTable']);
+
+app.controller('TablaController', function($scope, ngTableParams) {
+    $scope.dataPaginada= [];
+  $scope.data = [{nombre: "Nico", edad:27, sexo:'M'},
+  {nombre: "Martin", edad: 40,sexo:'M'},
+  {nombre: "Fidev", edad: 30,sexo:'M'},
+  {nombre: "Gonza", edad: 25,sexo:'M'},
+  ]
+ 
+
+  $scope.tableParams = new ngTableParams({
+    page: 1, // muestra la primera pagina
+    count: 3, // contador de elementos por pagina
+    }, {
+    counts:[3,4],
+    total: $scope.data.length, // length of data
+    getData: function (params) {
+        
+        $scope.dataPaginada = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+    return $scope.dataPaginada
+    } 
+    })
+
+
+});
